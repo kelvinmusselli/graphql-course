896ead186b4980e913cdb6512ee16efee5e49ac2
Kelvin Musselli <kelvin.serva@arquivei.com.br>
2023-03-26 19:39:42 -0300

    feat(graphql): query variables

--- src/graphql/user/typedefs.js
+++ src/graphql/user/typedefs.js
@@ -1,6 +1,27 @@
 import { gql } from 'apollo-server';
 
 export const userTypeDefs = gql`
+  # query variables
+  # query getUser($id: ID!, $teste: Boolean) {
+  #   user(
+  #     id: $id,
+  #     teste: $teste
+  #   ) {
+  #     userName
+  #     id
+  #   }
+  # }
+
+  #   {
+  #   "id": "602",
+  #   "teste": true
+  # }
+
+  # extend type Query {
+  #   user(id: ID!, teste: Boolean): User!
+  #   users: [User!]!
+  # }
+
   extend type Query {
     user(id: ID!): User!
     users: [User!]!
